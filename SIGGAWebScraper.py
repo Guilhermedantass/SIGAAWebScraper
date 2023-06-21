3621b99dc01803190461cb956d6ed5889bb17f84
guilhermedantass <guilhermedantas.dev@gmail.com>
2023-06-20 22:42:14 -0300

    update em obter_nota

--- SIGGAWebScraper.py
+++ SIGGAWebScraper.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 class SIGAAWebScraper:
     def __init__(self):
         self.options = Options()
+        self.options.add_argument("--headless=new")
         self.options.add_experimental_option("detach", True)
         self.navegador = webdriver.Chrome(options=self.options)
 
@@ -26,8 +27,24 @@ class SIGAAWebScraper:
         elem_cm_submenu.click()
 
     def obter_nota(self):
-        elem_nota = self.navegador.find_element(By.XPATH, '//*[@id="relatorio"]/div/table/tbody').text
-        return elem_nota
+        nota = self.navegador.find_element(By.XPATH, '//*[@id="relatorio"]/div/table/tbody').text
+        nota = nota.replace("--", "0")
+        nota = nota.split("\n")
+        lista = ['0','1','2 ', '3', '4' , '5' , '6' , '7' , '8' , '9']
+        for i in nota:
+            j = 8
+            for n in i[8:]:
+                if n not in lista:
+                    j += 1
+                else:
+                    break
+            print(i[8:j])
+            notas = i[j:].split(' ')
+            print(f'Primeira unidade: {notas[0]}')
+            print(f'Segunda unidade: {notas[1]}')
+            print(f'Terceira unidade: {notas[2]}')
+            print('----------------------------------------------------------------')
+        return nota
 
     def fechar_navegador(self):
         self.navegador.quit()
@@ -37,6 +54,5 @@ class SIGAAWebScraper:
 scraper = SIGAAWebScraper()
 scraper.fazer_login("", "")
 scraper.acessar_pagina_nota()
-nota = scraper.obter_nota()
-print(nota)
+scraper.obter_nota()
 scraper.fechar_navegador()
